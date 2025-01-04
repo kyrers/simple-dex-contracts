917249256dae6720df6b5a40b0e7da58d07e24f7
kyrers <kyrers.eth@gmail.com>
2025-01-04 17:24:46 +0000

    Create token contracts

---
+++ contracts/tokens/TokenB.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GNU General Public License v3.0
+pragma solidity ^0.8.28;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TokenB is ERC20, ERC20Permit, Ownable {
+    constructor(
+        address initialOwner
+    ) ERC20("TokenB", "TKB") ERC20Permit("TokenB") Ownable(initialOwner) {}
+}
