1db165e960270b6b331f69483b6c351ef1806e00
kyrers <kyrers.eth@gmail.com>
2025-01-09 16:50:47 +0000

    Use npm instead of pnpm to help with some hardhat type errors

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,6 @@ import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-ignition-ethers";
 
-
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
   networks: {
