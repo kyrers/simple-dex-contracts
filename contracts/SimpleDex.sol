25dab3dfba23a906bcd14a981028d253881499bd
kyrers <kyrers.eth@gmail.com>
2025-01-28 20:25:29 +0000

    Contract improvements + update hardhat version

--- contracts/SimpleDex.sol
+++ contracts/SimpleDex.sol
@@ -4,6 +4,11 @@ pragma solidity ^0.8.28;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
+/**
+ * @title Simple Dex
+ * @author kyrers
+ * @notice A simple dex that uses Tokens A and B, allow users to add or remove liquidity and swap between tokens. Fee on swaps is 0.3%.
+ */
 contract SimpleDex is ReentrancyGuard {
     IERC20 public tokenA;
     IERC20 public tokenB;
@@ -28,18 +33,12 @@ contract SimpleDex is ReentrancyGuard {
 
     error InsufficientLiquidity();
     error InsufficientLpTokens();
+    error InvalidBurnAmount(uint256 lpTokensToBurn);
     error InvalidSwapAmount(uint256 amount);
     error InvalidToken(address token);
     error InvalidTokenAddress(address tokenA, address tokenB);
     error InvalidTokenAmount(uint256 amountA, uint256 amountB);
     error InvalidTokenRatio(uint256 amountB, uint256 expectedAmountB);
-
-    modifier validTokenAmounts(uint256 amountA, uint256 amountB) {
-        if (amountA <= 0 || amountB <= 0) {
-            revert InvalidTokenAmount(amountA, amountB);
-        }
-        _;
-    }
 
     constructor(IERC20 _tokenA, IERC20 _tokenB) {
         if (address(_tokenA) == address(0) || address(_tokenB) == address(0)) {
@@ -50,17 +49,32 @@ contract SimpleDex is ReentrancyGuard {
         tokenB = _tokenB;
     }
 
+    /**
+     * @notice Add liquidity to the swap.
+     * @dev The first user to add liquidity sets the ratio. Subsequent users are forced to keep the ratio.
+     * @param amountA The amount of A tokens to add
+     * @param amountB The amount of B tokens to add
+     */
     function addLiquidity(
         uint256 amountA,
         uint256 amountB
-    ) external validTokenAmounts(amountA, amountB) nonReentrant {
+    ) external nonReentrant {
+        if (amountA <= 0 || amountB <= 0) {
+            revert InvalidTokenAmount(amountA, amountB);
+        }
+
         uint256 lpTokensMinted;
 
         if (totalLpTokens == 0) {
             // Initial liquidity
             lpTokensMinted = sqrt(amountA * amountB);
         } else {
-            validateRatio(amountA, amountB);
+            //Validate ratio
+            uint256 expectedAmountB = (amountA * reserveB) / reserveA;
+            if (amountB != expectedAmountB) {
+                revert InvalidTokenRatio(amountB, expectedAmountB);
+            }
+
             lpTokensMinted = (amountA * totalLpTokens) / reserveA;
         }
 
@@ -75,19 +89,21 @@ contract SimpleDex is ReentrancyGuard {
         emit AddLiquidity(msg.sender, amountA, amountB);
     }
 
-    function removeLiquidity(
-        uint256 amountA,
-        uint256 amountB
-    ) external validTokenAmounts(amountA, amountB) nonReentrant {
-        if (amountA > reserveA || amountB > reserveB) {
-            revert InsufficientLiquidity();
+    /**
+     * @notice Allows LPs to remove total or partial liquidity provided.
+     * @param lpTokensToBurn The amount of liquidity tokens to burn
+     */
+    function removeLiquidity(uint256 lpTokensToBurn) external nonReentrant {
+        if (lpTokensToBurn <= 0) {
+            revert InvalidBurnAmount(lpTokensToBurn);
         }
-        validateRatio(amountA, amountB);
 
-        uint256 lpTokensToBurn = (amountA * totalLpTokens) / reserveA;
         if (lpBalances[msg.sender] < lpTokensToBurn) {
             revert InsufficientLpTokens();
         }
+
+        uint256 amountA = (lpTokensToBurn * reserveA) / totalLpTokens;
+        uint256 amountB = (lpTokensToBurn * reserveB) / totalLpTokens;
 
         reserveA -= amountA;
         reserveB -= amountB;
@@ -100,22 +116,11 @@ contract SimpleDex is ReentrancyGuard {
         emit RemoveLiquidity(msg.sender, amountA, amountB);
     }
 
-    function validateRatio(uint256 amountA, uint256 amountB) private view {
-        uint256 expectedAmountB = (amountA * reserveB) / reserveA;
-        if (amountB != expectedAmountB) {
-            revert InvalidTokenRatio(amountB, expectedAmountB);
-        }
-    }
-
-    function sqrt(uint256 x) private pure returns (uint256 y) {
-        uint256 z = (x + 1) / 2;
-        y = x;
-        while (z < y) {
-            y = z;
-            z = (x / z + z) / 2;
-        }
-    }
-
+    /**
+     * @notice Swap between Token A and B. Constant 0.3% fee.
+     * @param tokenIn The token to swap
+     * @param amountIn The amount to swap
+     */
     function swap(address tokenIn, uint256 amountIn) external nonReentrant {
         if (amountIn <= 0) {
             revert InvalidSwapAmount(amountIn);
@@ -154,6 +159,13 @@ contract SimpleDex is ReentrancyGuard {
         emit Swap(msg.sender, tokenIn, amountIn, address(outToken), amountOut);
     }
 
+    /**
+     * @notice Calculates the amount of tokens to send to an user during a swap in order to keep the constant ratio.
+     * @param amountIn The amount of Token A or B sent
+     * @param reserveIn The reserves of the token sent
+     * @param reserveOut The reserves of the token to send
+     * @return Tokens to send to the user
+     */
     function getAmountOut(
         uint256 amountIn,
         uint256 reserveIn,
@@ -163,5 +175,20 @@ contract SimpleDex is ReentrancyGuard {
         uint256 numerator = amountInWithFee * reserveOut;
         uint256 denominator = (reserveIn * 1000) + amountInWithFee;
         return numerator / denominator;
+    }
+
+    /**
+     * @notice Calculates the square root of a number
+     * @dev Used to calculate initial LP tokens when first adding liquidity
+     * @param x The number to calculate the square root of
+     * @return y The square root of x
+     */
+    function sqrt(uint256 x) private pure returns (uint256 y) {
+        uint256 z = (x + 1) / 2;
+        y = x;
+        while (z < y) {
+            y = z;
+            z = (x / z + z) / 2;
+        }
     }
 }
